20844df5af5c7f45db51d9e58f37ee7609e13b00
Leocarnauba <leocarmacedo@hotmail.com>
2025-01-09 15:17:35 -0300

    Desktop Commit

---
+++ src/main/java/com/carnacorp/TrelloPlugin/TrelloPluginApplication.java
@@ -0,0 +1,13 @@
+package com.carnacorp.TrelloPlugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloPluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carnacorp/TrelloPlugin/TrelloPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carnacorp.TrelloPlugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
